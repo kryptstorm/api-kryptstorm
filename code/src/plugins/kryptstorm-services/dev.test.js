51db27d370603e65dea09571576661494a3b0405
Kryptstorm <kryptstorm@gmail.com>
2017-08-02 22:01:40 +0700

    Add basic test case for kryptstorm-service

--- code/src/plugins/kryptstorm-services/dev.test.js
+++ code/src/plugins/kryptstorm-services/dev.test.js
@@ -1,6 +1,7 @@
 /** External modules */
 import Seneca from "seneca";
 import { expect } from "chai";
+import _ from "lodash";
 
 /** Internal modules */
 import KryptstormService from ".";
@@ -19,25 +20,50 @@ app.add("services:test", function(args, done) {
 });
 
 describe("Kryptstorm Service", function() {
+  const payload = { param: "param" };
+  const _basic = (done, { errorCode$ = "ERROR_NONE", data$ }) => {
+    /** Error is ERROR_NONE */
+    expect(errorCode$).to.be.equal("ERROR_NONE");
+    /** Data must be array of item */
+    expect(data$).to.be.exist;
+    expect(data$).to.be.an("object");
+    expect(data$.param).to.be.equal(payload.param);
+    return done();
+  };
+
   /**
 	 * Prepare before run all unit test
 	 * After before function, app is ready to call action
 	 */
   before(done => app.ready(() => done()));
 
-  it("Basic", function(done) {
-    const payload = { param: "param" };
+  it("Service - use pattern as a string, payload as an object", function(done) {
     app
       .asyncAct$("services:test", payload)
-      .then(({ errorCode$ = "ERROR_NONE", data$ }) => {
-        /** Error is ERROR_NONE */
-        expect(errorCode$).to.be.equal("ERROR_NONE");
-        /** Data must be array of item */
-        expect(data$).to.be.exist;
-        expect(data$).to.be.an("object");
-        expect(data$.param).to.be.equal(payload.param);
-        return done();
-      })
+      .then(_basic.bind(null, done))
+      .catch(done);
+  });
+
+  it("Service - use pattern as an object, payload as an object", function(
+    done
+  ) {
+    app
+      .asyncAct$({ services: "test" }, payload)
+      .then(_basic.bind(null, done))
+      .catch(done);
+  });
+
+  it("Service - use pattern & payload as a string", function(done) {
+    app
+      .asyncAct$("services:test, " + app.util.pattern(payload))
+      .then(_basic.bind(null, done))
+      .catch(done);
+  });
+
+  it("Service - use pattern & payload as a object", function(done) {
+    app
+      .asyncAct$(_.assign({ services: "test" }, payload))
+      .then(_basic.bind(null, done))
       .catch(done);
   });
 });
--- code/src/plugins/kryptstorm-services/index.js
+++ code/src/plugins/kryptstorm-services/index.js
@@ -1,12 +1,18 @@
 /** External modules */
 import Bluebird from "bluebird";
+import _ from "lodash";
 
 export default function Services({ services = {}, before = [], after = [] }) {
   const asyncAct$ = Bluebird.promisify(this.act, { context: this });
-  console.log(this.prototype);
 
   /** Register async act */
-  this.decorate("asyncAct$", asyncAct$);
+  this.decorate("asyncAct$", (pattern, params) => {
+    /** Convert object to string */
+    if (_.isObject(pattern)) pattern = this.util.pattern(pattern);
+    if (!_.isObject(params)) params = {};
+
+    return asyncAct$(pattern, params);
+  });
 
   this.add("init:Services", function initServices(args, done) {
     return done();
