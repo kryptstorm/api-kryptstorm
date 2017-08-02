7eb4deb2374ace640c2f0ca43382b3a17cdb1b73
Kryptstorm <kryptstorm@gmail.com>
2017-08-02 22:07:54 +0700

    Add prepare services

--- code/src/plugins/kryptstorm-services/index.js
+++ code/src/plugins/kryptstorm-services/index.js
@@ -21,6 +21,23 @@ export default function Services({ services = {}, before = [], after = [] }) {
   return { name: "Services" };
 }
 
+const _prepareServices = services =>
+  _.reduce(
+    services,
+    (_services, pattern, info) => {
+      if (!_.isString(pattern)) return _services;
+      if (_.isString(info)) {
+        return (_services[pattern] = {
+          method: "get",
+          url: info
+        });
+      }
+      if (!_.isObject(info) || !_.isString(method.method) || !_.isString(url))
+        return _services;
+      return (_services[pattern] = info);
+    },
+    {}
+  );
 const ex = {
   "users:find_all": {
     method: "get",
