a243a4695c4041ce0a24a265f5dbcc8dba8dfd1f
Kryptstorm <kryptstorm@gmail.com>
2017-08-03 22:00:31 +0700

    Add test case for both global and local hooks

--- code/src/plugins/kryptstorm-services/dev.test.js
+++ code/src/plugins/kryptstorm-services/dev.test.js
@@ -52,7 +52,7 @@ describe("Basic", function() {
   });
 });
 
-describe("Using Hooks", function() {
+describe("Using global hooks", function() {
   /** Init test app */
   const app = TestApp();
   const payload = { param: "param" };
@@ -73,13 +73,13 @@ describe("Using Hooks", function() {
 
     /** Register service */
     app.add("services:test, global:before", function(args, done) {
-      return done(null, { data$: { before: "global", param: args.param } });
+      return done(null, { data$: { "global-hook-before": args.param } });
     });
     app.add("services:test", function(args, done) {
-      return done(null, { data$: { services: "test", param: args.param } });
+      return done(null, { data$: { test: args.param } });
     });
     app.add("services:test, global:after", function(args, done) {
-      return done(null, { data$: { after: "global", param: args.param } });
+      return done(null, { data$: { "global-hook-after": args.param } });
     });
 
     app.ready(() => done());
@@ -94,9 +94,70 @@ describe("Using Hooks", function() {
         /** Data must be array of item */
         expect(data$).to.be.exist;
         expect(data$).to.be.an("object");
-        expect(data$.before).to.be.equal("global");
-        expect(data$.after).to.be.equal("global");
-        expect(data$.param).to.be.equal(payload.param);
+        expect(data$["global-hook-before"]).to.be.equal(payload.param);
+        expect(data$.test).to.be.equal(payload.param);
+        expect(data$["global-hook-after"]).to.be.equal(payload.param);
+        return done();
+      })
+      .catch(done);
+  });
+});
+
+describe("Using both global and local hooks", function() {
+  /** Init test app */
+  const app = TestApp();
+  const payload = { param: "param" };
+
+  /**
+	 * Prepare before run all unit test
+	 * After before function, app is ready to call action
+	 */
+  before(done => {
+    app.use(KryptstormService, {
+      beforeHooks: {
+        global: ["services:test, global:before"],
+        "services:test": ["services:test, local:before"]
+      },
+      afterHooks: {
+        global: ["services:test, global:after"],
+        "services:test": ["services:test, local:after"]
+      }
+    });
+
+    /** Register service */
+    app.add("services:test, global:before", function(args, done) {
+      return done(null, { data$: { "global-hook-before": args.param } });
+    });
+    app.add("services:test, local:before", function(args, done) {
+      return done(null, { data$: { "local-hook-before": args.param } });
+    });
+    app.add("services:test", function(args, done) {
+      return done(null, { data$: { test: args.param } });
+    });
+    app.add("services:test, local:after", function(args, done) {
+      return done(null, { data$: { "local-hook-after": args.param } });
+    });
+    app.add("services:test, global:after", function(args, done) {
+      return done(null, { data$: { "global-hook-after": args.param } });
+    });
+
+    app.ready(() => done());
+  });
+
+  it("Global hooks", function(done) {
+    app
+      .asyncAct$("services:test", payload)
+      .then(({ errorCode$ = "ERROR_NONE", data$ }) => {
+        /** Error is ERROR_NONE */
+        expect(errorCode$).to.be.equal("ERROR_NONE");
+        /** Data must be array of item */
+        expect(data$).to.be.exist;
+        expect(data$).to.be.an("object");
+        expect(data$["global-hook-before"]).to.be.equal(payload.param);
+        expect(data$["local-hook-before"]).to.be.equal(payload.param);
+        expect(data$.test).to.be.equal(payload.param);
+        expect(data$["local-hook-after"]).to.be.equal(payload.param);
+        expect(data$["global-hook-after"]).to.be.equal(payload.param);
         return done();
       })
       .catch(done);
